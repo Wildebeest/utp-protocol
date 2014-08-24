2948e57c701fd17a32bf122d31d8ae7fa9e6af7e
Matthew Wilde <wilde@outlook.com>
2014-08-24 08:52:04 -0700

    Play around with UDP sockets.

--- index.js
+++ index.js
@@ -3,10 +3,56 @@ module.exports = uTP;
 var inherits = require('inherits');
 var stream = require('stream');
 var dgram = require('dgram');
+var process = require('process');
 
 inherits(uTP, stream.Duplex);
+
+// Type constants
+var ST_DATA = 0;
+var ST_FIN = 1;
+var ST_STATE = 2;
+var ST_RESET = 3;
+var ST_SYN = 4;
+
+var VERSION = 1;
+
+function rand16() {
+	return Math.floor(Math.random() * 65536);
+}
 
 function uTP () {
 	stream.Duplex.call(this);
 
+	this._currentWindow = 0;
+	this._maxWindow = 0;
+	this._windowSize = 0;
+	this._replyMicroseconds = 0;
+	this._sequenceNumber = 1;
+	this._ackNumber = 0;
+
+
+	var socket = this._socket = dgram.createSocket('udp4');
+	socket.on('message', this._onMessage.bind(this));
 }
+
+uTP.prototype.listen = function (port, host, callback) {
+	this._socket.bind(port, host, callback);
+};
+
+uTP.prototype.close = function () {
+	this._socket.close();
+};
+
+uTP.prototype._onMessage = function (msg, rinfo) {
+	console.log("received message \nmsg:\n%j\nrinfo:\n%j", msg, rinfo)
+};
+
+// Readable implementation
+uTP.prototype._read = function (size) {
+
+}
+
+// Writable implementation
+uTP.prototype._write = function (chunk, encoding, callback) {
+
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 var uTP = require('../');
 var test = require('tape');
+var dgram = require('dgram');
 
 test('constructor', function (t) {
 	t.plan(1);
 
 	var stream = new uTP();
-	t.ok(stream);
+	stream.listen(1337);
+
+	var message = new Buffer("hello UDP");
+	var socket = dgram.createSocket('udp4');
+	socket.send(message, 0, message.length, 1337, "localhost");
+
+	setTimeout(
+		function (err, bytes) {
+			t.ok(stream);
+			socket.close();
+			stream.close();
+		}, 1000);
 });
